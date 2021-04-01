53021d242e42f3d8afea4881bd45d6a41cd8bf1d
svrchi <kevinsarchi@yahoo.com>
2021-03-31 20:54:37 -0400

    frontend adjustments

--- src/Auth/LoginForm.js
+++ src/Auth/LoginForm.js
@@ -1,78 +1,101 @@
+/* eslint-disable no-console */
 // rafce imports React, exports and creates function with ES7 React/Redux/GraphQL/React-Native snippets extention
 import { TextField } from '@material-ui/core';
-import Button from "@material-ui/core/Button";
-import { withStyles } from "@material-ui/core/styles";
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 import React from 'react';
 import './LoginForm.css';
 
-const styles = theme => ({
-    input: {
-      height: 40
-    },
-    button: {
-      height: 10
-    }
-  });
-
-
-const LoginForm = withStyles(styles) (props => {
-    const { classes } = props;
-    
-    const [email, setEmail] = React.useState('')
-    const [password, setPassword] = React.useState('')
-   
-     const handleChange = (e, type) =>{
-           e.preventDefault();
-           if(type === 'email') {
-             setEmail(e.target.value)
-           }
-           if(type==='password') {
-             setPassword(e.target.value)
-           }
-     }
-
-     const buttonClick = () => {
-       console.log('E-Mail: ', email , ' Password: ', password)
-     }
-
-    return (
-        <div className="loginForm"> 
-             <TextField 
-             className="UNForm" 
-             label="Email" 
-             variant="filled" 
-             size="small" 
-             InputProps={{className: classes.input }}
-             InputLabelProps={{shrink: true}}
-             value={email}
-             onChange={e => handleChange(e, 'email')}
-             />
-             {/* hide password */}
-             <TextField 
-             className="PWForm" 
-             label="Password" 
-             type='password'
-             variant="filled" 
-             size="small" 
-             height="50%" 
-             InputProps={{className: classes.input }} 
-             InputLabelProps={{shrink: true}}
-             value={password}
-             onChange={e => handleChange(e, 'password')}
-             />
-              <Button className="loginButton" variant="contained"size="small" style={{width:'90%'}} onClick={buttonClick}>
-                 Log in
-              </Button>
-
-             <div className="loginAnchors" >
-              <span className="forgotPassword">Forgot password?</span>
-              <span className="signUpAnchor"  onClick={console.log('testing sign up')}>
-                Sign up
-              </span>
-              
-            </div>
-        </div>
-    )
+const styles = (theme) => ({
+  input: {
+    height: 40,
+  },
+  button: {
+    height: 10,
+  },
 });
 
-export default LoginForm
+const LoginForm = withStyles(styles)((props) => {
+  const { classes } = props;
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleChange = (e, type) => {
+    e.preventDefault();
+    if (type === 'email') {
+      setEmail(e.target.value);
+    }
+    if (type === 'password') {
+      setPassword(e.target.value);
+    }
+  };
+
+  const onSubmit = () => {
+    console.log('E-Mail: ', email, ' Password: ', password);
+    fetch('http://localhost:3001/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(
+        {
+          user: {
+            email,
+            password,
+          },
+        },
+      ),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
+  return (
+    <div className="loginForm">
+      <TextField
+        className="UNForm"
+        label="Email"
+        variant="filled"
+        size="small"
+        InputProps={{ className: classes.input }}
+        InputLabelProps={{ shrink: true }}
+        value={email}
+        onChange={(e) => handleChange(e, 'email')}
+      />
+      {/* hide password */}
+      <TextField
+        className="PWForm"
+        label="Password"
+        type="password"
+        variant="filled"
+        size="small"
+        height="50%"
+        InputProps={{ className: classes.input }}
+        InputLabelProps={{ shrink: true }}
+        value={password}
+        onChange={(e) => handleChange(e, 'password')}
+      />
+      <Button
+        className="loginButton"
+        variant="contained"
+        size="small"
+        onClick={onSubmit}
+      >
+        Log in
+      </Button>
+
+      <div className="loginAnchors">
+        <button className="forgotPassword" type="button">Forgot password?</button>
+        <button className="signUpAnchor" type="button" onClick={console.log('testing sign up')}>
+          Sign up
+        </button>
+
+      </div>
+    </div>
+  );
+});
+
+export default LoginForm;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { BrowserRouter as Link, Route, Router } from 'react-router-dom';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import './Header.css';
+
+// const Header = () => (
+//   <nav className="header">
+//     <ul className="list-items">
+//       <li>
+//       <h3 className="title">Bow-Wow</h3>
+//       </li>
+//       <li>
+//       <Link to="/home"><h3>Home</h3></Link>
+//       </li>
+//       <li>
+//         <Link to="/">
+//           {' '}
+//           <h3>Login</h3>
+//           {' '}
+//         </Link>
+//       </li>
+//       <li>
+//       <AddCircleIcon />
+//       </li>
+//       <li>
+//       <AccountCircleIcon />
+//       </li>
+//     </ul>
+//   </nav>
+// );
+
+ const Header = () => (
+<nav className="header"> 
+<ul className="list-items" >
+<li>
+  <Link to="/home"><h2>Bow-Wow</h2></Link>
+</li> 
+  <div className="headerAnchors">
+    <li>
+      <Link to="/">
+        {' '}
+         <AccountCircleIcon className="headerIcons" style={{ fontSize: 60 }}/> 
+        {' '}
+      </Link>
+      </li>
+    <li>
+  <AddCircleIcon className="headerIcons" style={{ fontSize: 60 }}/>
+  </li>
+  </div>
+  
+</ul>
+ 
+
+   </nav>
+ );
+
+export default Header;
