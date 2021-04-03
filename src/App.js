1a1eb6d771445572a840e035ff295daf4ff8fdf5
svrchi <kevinsarchi@yahoo.com>
2021-04-03 11:40:42 -0400

    final UI touch ups

--- src/App.js
+++ src/App.js
@@ -1,55 +1,53 @@
 import './App.css';
 import React from 'react';
-import Favorites from './containers/Favorites/Favorites'
-import Home from './containers/Home'
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
+import Favorites from './containers/Favorites/Favorites';
+import Home from './containers/Home';
 import SignIn from './containers/SignIn/SignIn';
 import Header from './components/Header/Header';
-import AddDog from './components/AddDog/AddDog'
-import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
-
+import AddDog from './components/AddDog/AddDog';
 
 function App() {
-
-  const [userId, setUserId] = React.useState()
+  const [userId, setUserId] = React.useState();
 
   const addFav = (dogData) => {
-      fetch(`http://localhost:3001/users/${userId}/dogs/${dogData}`, {
-        method: 'POST',
-      })
-      .catch(err => console.log)
+    fetch(`http://localhost:3001/users/${userId}/dogs/${dogData}`, {
+      method: 'POST',
+    })
+      .catch((err) => console.log)
       .then((res) => res.json())
       .then((res) => {
-        console.log(res)
+        console.log(res);
       });
   };
 
   const addUserId = (userId) => {
-    setUserId(userId)
-  }
+    setUserId(userId);
+  };
 
-  console.log(userId)
+  console.log(userId);
 
-  let Auth = <Route exact path="/" render={() => <SignIn addUserId={addUserId}/>} />
-  let HomeComp = <Redirect to="/" />
-  let Favorite = <Redirect to="/" />
-  let addDog = <Redirect to="/" />
+  let Auth = <Route exact path="/" render={() => <SignIn addUserId={addUserId} />} />;
+  let HomeComp = <Redirect to="/" />;
+  let Favorite = <Redirect to="/" />;
+  let addDog = <Redirect to="/" />;
 
-  if(userId) {
-    Auth = <Redirect to="/home" />
-    HomeComp = <Route exact path="/home" render={()=> < Home userId={userId} addFav={addFav} />}  />
-    Favorite = <Route exact path="/favorites" render={() => <Favorites userId={userId}/>}/>
-    addDog = <Route exact path="/addDog" render={()=><AddDog userId={userId}/>} />
+  if (userId) {
+    Auth = <Redirect to="/home" />;
+    HomeComp = <Route exact path="/home" render={() => <Home userId={userId} addFav={addFav} />} />;
+    Favorite = <Route exact path="/favorites" render={() => <Favorites userId={userId} />} />;
+    addDog = <Route exact path="/addDog" render={() => <AddDog userId={userId} />} />;
   }
 
   return (
     <div className="App">
       <Router>
         <Header />
-            {Auth}
-            {HomeComp}
-            {Favorite}
-            {addDog}
-      </Router> 
+        {Auth}
+        {HomeComp}
+        {Favorite}
+        {addDog}
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,37 +5,37 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import HomeIcon from '@material-ui/icons/Home';
 import './Header.css';
 
- const Header = () => (
-<nav className="header"> 
-<ul className="list-items" >
-  
-    <li>
-    
-    <div>
-      <h2>Bow-Wow</h2>
-    </div>
-    
-    </li> 
-  
-  <div className="headerAnchors">
-    <li>
-    <Link to="/home" style={{textDecoration: 'none', color: '#3B2826'}}>
-          <HomeIcon className="headerIcons" style={{ fontSize: 55 }}/>
-    </Link>
-    </li>
-    <li>
-      <Link to="/favorites">
-        {' '}
-         <AccountCircleIcon className="headerIcons" style={{ fontSize: 55 }}/> 
-        {' '}
-       
-      </Link>
+const Header = () => (
+  <nav className="header">
+    <ul className="list-items">
+
+      <li>
+
+        <div>
+          <h2>Bow-Wow</h2>
+        </div>
+
       </li>
+
+      <div className="headerAnchors">
         <li>
-        <Link to="/addDog">
+          <Link to="/home" style={{ textDecoration: 'none', color: '#3B2826' }}>
+            <HomeIcon className="headerIcons" style={{ fontSize: 55 }} />
+          </Link>
+        </li>
+        <li>
+          <Link to="/favorites">
             {' '}
-          <AddCircleIcon className="headerIcons" style={{ fontSize: 55 }} />
-          {' '}
+            <AccountCircleIcon className="headerIcons" style={{ fontSize: 55 }} />
+            {' '}
+
+          </Link>
+        </li>
+        <li>
+          <Link to="/addDog">
+            {' '}
+            <AddCircleIcon className="headerIcons" style={{ fontSize: 55 }} />
+            {' '}
 
           </Link>
         </li>
